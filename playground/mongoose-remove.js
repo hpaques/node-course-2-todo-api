d5ca08d60625118eb9d5b267f5d2088cfaa8ea77
Henrique Paques <paques@gmail.com>
2018-02-12 15:14:45 -0500

    Add DELETE /todos/:id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,13 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+//   console.log(result);
+// });
+
+Todo.findByIdAndRemove('5a81ee5c4fc4a50d2dfe460e').then((doc) => {
+  console.log(doc);
+});
--- server/server.js
+++ server/server.js
@@ -56,8 +56,27 @@ app.get('/todos/:id', (req, res) => {
     res.send({todo});
   }, (e) => {
     console.log('another issue');
-    res.status(404).send();
+    res.status(400).send();
   });
+});
+
+// DELETE ROUTE
+app.delete('/todos/:id', (req,res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send(todo);
+  }).catch((e) => {
+    res.status(404).send();
+  })
 });
 
 app.listen(port, () => {
